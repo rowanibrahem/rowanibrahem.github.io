685702e77851b9c48eef0afc4a1e9ee3ee1226dc
Rowan Ibrahem <rowanibrahem2003@gmail.com>
2024-12-05 19:55:22 +0200

    Deploy Flutter web ap19

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3109700535"
+    serviceWorkerVersion: "808950770"
   }
 });
